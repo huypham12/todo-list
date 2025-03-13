5e8e27937d377c481f430f5b4350ece6fda9b191
HuyPham <huy9008437@mail.com>
2025-03-13 11:44:44 +0700

    uri

--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongo_uri = 'mongodb://localhost:27017/todo-list-db'
+const mongo_uri = 'mongodb+srv://huypham:123@todo-db.whz9e.mongodb.net/'
 
 const connect = async ()=>{
   try{
